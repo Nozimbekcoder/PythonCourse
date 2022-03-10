e25d2f7615a8c38921ad609ed65b62280d6dd3f2
Nozimbek <87319225+Nozimbekcoder@users.noreply.github.com>
2022-03-10 10:43:27 +0500

    fevral oxiri

---
+++ 2-oy/Test/topshiriqlar.py
@@ -0,0 +1,34 @@
+# 1. Uchta butun son berilgan. Shu sonlarni kichigini aniqlaydigan dastur tuzing
+#
+# 2. Butun son berilgan. Berilgan sonni 'Musbat toq son' ,
+# 'Manfiy juft son', 'son nolga teng' va xokazo ekranga chiqaradigan
+# dastur tuzing
+#
+# 3. Bir kg konfetning narxi berilgan (float). 1,2, ..., 9, 10 kg konfetni
+# narxini chiqaruvchi dastur tuzing
+#
+# 4. a va b butun sonlar berilgan (a < b). a dan b gacha bo'lgan sonlarning
+# yigindisini ekranga chiqaruvchi dastur tuzing.
+#
+# 5. Foydalanuvchidan bugun nechta odam bilan
+# uchrashganini (suhbatlashganini) so'rang, va har bir suhbatlashgan
+# odamning ismini birma-bir so'rab ro'yxatga yozing.
+# Ro'yxatni konsolga chiqaring.
+
+
+son = input("Son kiriting: ")
+
+try:
+    son = int(son)
+    if son > 0:
+        if son % 2 == 0:
+            print(son, "Musbat juft son")
+        else:
+            print(son, "Musbat toq son")
+    else:
+        if son % 2 == 0:
+            print(son, "Manfiy juft son")
+        else:
+            print(son, "Manfiy toq son")
+except:
+    print("Tog'ri javob kiritmadingiz! ")
